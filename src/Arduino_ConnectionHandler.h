31ff19e86970546cc26ae517c46ecb689d467630
Alexander Entinger <cto@lxrobotics.com>
2021-03-16 14:21:56 +0100

    Adding missing closing brace.

--- src/Arduino_ConnectionHandler.h
+++ src/Arduino_ConnectionHandler.h
@@ -30,7 +30,7 @@
 #endif
 
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || \
-  defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined (ARDUINO_NANO_RP2040_CONNECT
+  defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined (ARDUINO_NANO_RP2040_CONNECT)
   #include <WiFiNINA.h>
   #include <WiFiUdp.h>
 
