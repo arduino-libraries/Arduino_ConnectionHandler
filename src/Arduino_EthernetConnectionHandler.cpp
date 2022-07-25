a3af901c5b83e5c93318e93697aa4f50d1e4d8d5
pennam <m.pennasilico@arduino.cc>
2022-07-25 13:45:27 +0200

    Handle automatic retries on connect/disconnect

--- src/Arduino_EthernetConnectionHandler.cpp
+++ src/Arduino_EthernetConnectionHandler.cpp
@@ -36,46 +36,61 @@ EthernetConnectionHandler::EthernetConnectionHandler(bool const keep_alive)
 
 NetworkConnectionState EthernetConnectionHandler::update_handleInit()
 {
-  if (Ethernet.begin() == 0) {
-    Debug.print(DBG_ERROR, F("Failed to configure Ethernet using DHCP"));
-
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-        Debug.print(DBG_ERROR, F("Error, ethernet shield was not found."));
-        return NetworkConnectionState::ERROR;
-    }
-
-    if (Ethernet.linkStatus() == LinkOFF) {
-        Debug.print(DBG_ERROR, F("Error, ethernet cable is not connected."));
-        return NetworkConnectionState::ERROR;
-    }
-
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+#if !defined(__AVR__)
+    Debug.print(DBG_ERROR, F("Error, ethernet shield was not found."));
+#endif
     return NetworkConnectionState::ERROR;
   }
-
   return NetworkConnectionState::CONNECTING;
 }
 
 NetworkConnectionState EthernetConnectionHandler::update_handleConnecting()
 {
+  if (Ethernet.begin(nullptr, 15000, 4000) == 0) {
+#if !defined(__AVR__)
+    Debug.print(DBG_ERROR, F("Waiting Ethernet configuration from DHCP server, check cable connection"));
+#endif
+    return NetworkConnectionState::CONNECTING;
+  }
   return NetworkConnectionState::CONNECTED;
 }
 
 NetworkConnectionState EthernetConnectionHandler::update_handleConnected()
 {
-  if (Ethernet.linkStatus() == LinkON)
-    return NetworkConnectionState::CONNECTED;
-  else
+  if (Ethernet.linkStatus() == LinkOFF) {
+#if !defined(__AVR__)
+    Debug.print(DBG_VERBOSE, F("Ethernet.status(): %d"), Ethernet.status());
+    Debug.print(DBG_ERROR, F("Connection lost."));
+#endif
+    if (_keep_alive)
+    {
+#if !defined(__AVR__)
+      Debug.print(DBG_ERROR, F("Attempting reconnection"));
+#endif
+    }
     return NetworkConnectionState::DISCONNECTED;
+  }
+  return NetworkConnectionState::CONNECTED;
 }
 
 NetworkConnectionState EthernetConnectionHandler::update_handleDisconnecting()
 {
+  Ethernet.disconnect();
   return NetworkConnectionState::DISCONNECTED;
 }
 
 NetworkConnectionState EthernetConnectionHandler::update_handleDisconnected()
 {
-  return NetworkConnectionState::INIT;
+  Ethernet.end();
+  if (_keep_alive)
+  {
+    return NetworkConnectionState::INIT;
+  }
+  else
+  {
+    return NetworkConnectionState::CLOSED;
+  }
 }
 
 #endif /* #ifdef BOARD_HAS_ETHERNET */
