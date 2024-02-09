8902e6e98f71146f10ac66b371e7a28e2f7f808d
Zachary J. Fields <zachary_fields@yahoo.com>
2024-02-09 11:28:51 -0600

    chore: LoRa to use BOARD_HAS_LORA
    Follows precedent established by Wi-Fi implementation.

--- src/Arduino_LoRaConnectionHandler.cpp
+++ src/Arduino_LoRaConnectionHandler.cpp
@@ -19,9 +19,9 @@
    INCLUDE
  ******************************************************************************/
 
-#if defined(ARDUINO_SAMD_MKRWAN1300) || defined(ARDUINO_SAMD_MKRWAN1310) /* Only compile if the board has LoRa */
-
 #include "Arduino_LoRaConnectionHandler.h"
+
+#if defined(BOARD_HAS_LORA) /* Only compile if the board has LoRa */
 
 /******************************************************************************
    TYPEDEF
@@ -106,7 +106,7 @@ NetworkConnectionState LoRaConnectionHandler::update_handleInit()
   {
     Debug.print(DBG_ERROR, F("Something went wrong; are you indoor? Move near a window, then reset and retry."));
     return NetworkConnectionState::ERROR;
-  } 
+  }
   // Set channelmask based on configuration
   if (_channelMask) {
     _modem.sendMask(_channelMask);
